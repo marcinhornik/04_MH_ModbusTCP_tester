a0349e626ae6e8b187404b9a03eb2e1399d7cb20
MarcinH <horas@o2.pl>
2017-12-13 21:45:20 +0100

    HEX2ASCII function tests

---
+++ ASCI_conversion.js
@@ -0,0 +1,30 @@
+// String to ASCII conversion function
+
+
+
+
+function hex2a(hexx) {
+	
+	
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    //return str;
+	
+	console.log(str);
+
+}
+
+function a2hex(str) {
+	
+  var arr = [];
+  for (var i = 0, l = str.length; i < l; i ++) {
+    var hex = Number(str.charCodeAt(i)).toString(16);
+    arr.push(hex);
+  }
+  //return arr.join('');
+  
+  console.log(arr.join(''));
+  
+}
